78289794f86174b2c5f65bcf5e030065e13f4d23
Areeb Ahmad <75535258+areeb3@users.noreply.github.com>
2022-11-12 10:46:52 +0530

    passed

--- passStrength.cpp
+++ passStrength.cpp
@@ -1,40 +1,24 @@
 // password checking
 
 #include<bits/stdc++.h>
-
 using namespace std;
 
 void findStrengthOfPass(string s){
-    bool upperChar=false;
-    bool lowerChar=false;
-    bool num=false;
-    bool specialChar=false;
+    bool upperChar=false, lowerChar=false, num=false, specialChar=false;
 
     for(int i=0;i<s.size();i++){
-        if(isupper(s[i])){
-            upperChar=true;
-        }
-        else if(islower(s[i])){
-            lowerChar=true;
-        }
-        else if(isdigit(s[i])){
-            num=true;
-        }
-        else if(!isalpha(s[i])){
-            specialChar=true;
-        }
+        if(isupper(s[i])) upperChar = true;
+        else if(islower(s[i])) lowerChar = true;
+        else if(isdigit(s[i])) num = true;
+        else if(!isalpha(s[i])) specialChar = true;
     }
 
-    if((upperChar) && (lowerChar) && (num) && (specialChar) && (s.size()>=6)){
-        cout<<"Strong Password\n";
-    }
-    else if((upperChar && lowerChar && specialChar && s.size()>=6) ||
-    (upperChar && lowerChar && num && s.size()>=6)){
+    if((upperChar) && (lowerChar) && (num) && (specialChar) && (s.size()>=6)) cout<<"Strong Password\n";
+
+    else if((upperChar && lowerChar && specialChar && s.size()>=6) ||( upperChar && lowerChar && num && s.size()>=6))
         cout<<"Moderate Password\n";
-    }
-    else{
-        cout<<"Weak Password\n";
-    }
+    
+    else cout<<"Weak Password\n";
 
     return ;
 }
